36eed5dbed095ea0150fd397af0f485fbb50c63b
Monisha101 <156109900+Monisha101@users.noreply.github.com>
2024-02-20 10:49:47 -0600

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,28 @@
+function settile () {
+    if (ntile == 0) {
+        tiles.setTileAt(tiles.getTileLocation(current_tile.column, current_tile.row), assets.tile`myTile0`)
+    }
+    if (ntile == 1) {
+        tiles.setTileAt(tiles.getTileLocation(current_tile.column, current_tile.row), assets.tile`myTile1`)
+    }
+    if (ntile == 2) {
+        tiles.setTileAt(tiles.getTileLocation(current_tile.column, current_tile.row), assets.tile`myTile2`)
+    }
+    if (ntile == 3) {
+        tiles.setTileAt(tiles.getTileLocation(current_tile.column, current_tile.row), assets.tile`myTile3`)
+    }
+    if (ntile == 4) {
+        tiles.setTileAt(tiles.getTileLocation(current_tile.column, current_tile.row), assets.tile`myTile4`)
+    }
+    if (ntile == 5) {
+        tiles.setTileAt(tiles.getTileLocation(current_tile.column, current_tile.row), assets.tile`myTile5`)
+    }
+}
+function join (color: number) {
+	
+}
 let ntile = 0
+let current_tile: tiles.Location = null
 tiles.setCurrentTilemap(tilemap`level3`)
 let lastrow = 6
 let lastcolumn = 7
@@ -8,11 +32,11 @@ let cursor = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . 1 1 1 1 1 . . . . . 
-    . . . . . 1 . . . . . 1 . . . . 
-    . . . . . 1 . . . . . 1 . . . . 
-    . . . . . 1 . . . . . 1 . . . . 
-    . . . . . 1 . . . . . 1 . . . . 
-    . . . . . 1 . . . . . 1 . . . . 
+    . . . . . 1 1 1 1 1 1 1 . . . . 
+    . . . . . 1 1 1 1 1 1 1 . . . . 
+    . . . . . 1 1 1 1 1 1 1 . . . . 
+    . . . . . 1 1 1 1 1 1 1 . . . . 
+    . . . . . 1 1 1 1 1 1 1 . . . . 
     . . . . . . 1 1 1 1 1 . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
@@ -21,36 +45,27 @@ let cursor = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
 tiles.placeOnTile(cursor, tiles.getTileLocation(0, 0))
-let current_tile = cursor.tilemapLocation()
+current_tile = cursor.tilemapLocation()
 let nlist = [
+0,
 1,
 2,
 3,
 4,
-5,
-6
+5
 ]
 tiles.placeOnTile(cursor, current_tile)
 for (let index = 0; index < 7; index++) {
     for (let index = 0; index < 8; index++) {
         ntile = nlist._pickRandom()
-        if (ntile == 1) {
-            tiles.setTileAt(tiles.getTileLocation(current_tile.column, current_tile.row), assets.tile`myTile0`)
+        settile()
+        while (tiles.tileImageAtLocation(tiles.getTileLocation(current_tile.column - 1, current_tile.row)) == tiles.tileImageAtLocation(tiles.getTileLocation(current_tile.column, current_tile.row))) {
+            ntile = nlist._pickRandom()
+            settile()
         }
-        if (ntile == 2) {
-            tiles.setTileAt(tiles.getTileLocation(current_tile.column, current_tile.row), assets.tile`myTile1`)
-        }
-        if (ntile == 3) {
-            tiles.setTileAt(tiles.getTileLocation(current_tile.column, current_tile.row), assets.tile`myTile2`)
-        }
-        if (ntile == 4) {
-            tiles.setTileAt(tiles.getTileLocation(current_tile.column, current_tile.row), assets.tile`myTile4`)
-        }
-        if (ntile == 5) {
-            tiles.setTileAt(tiles.getTileLocation(current_tile.column, current_tile.row), assets.tile`myTile5`)
-        }
-        if (ntile == 6) {
-            tiles.setTileAt(tiles.getTileLocation(current_tile.column, current_tile.row), assets.tile`myTile6`)
+        while (tiles.tileImageAtLocation(tiles.getTileLocation(current_tile.column, current_tile.row - 1)) == tiles.tileImageAtLocation(tiles.getTileLocation(current_tile.column, current_tile.row))) {
+            ntile = nlist._pickRandom()
+            settile()
         }
         tiles.placeOnTile(cursor, tiles.getTileLocation(current_tile.column + 1, current_tile.row))
         current_tile = cursor.tilemapLocation()
@@ -58,6 +73,12 @@ for (let index = 0; index < 7; index++) {
     tiles.placeOnTile(cursor, tiles.getTileLocation(0, current_tile.row + 1))
     current_tile = cursor.tilemapLocation()
 }
+tiles.setTileAt(tiles.getTileLocation(9, 0), assets.tile`myTile6`)
+tiles.setTileAt(tiles.getTileLocation(9, 1), assets.tile`myTile7`)
+tiles.setTileAt(tiles.getTileLocation(9, 2), assets.tile`myTile`)
+tiles.setTileAt(tiles.getTileLocation(9, 3), assets.tile`myTile8`)
+tiles.setTileAt(tiles.getTileLocation(9, 4), assets.tile`myTile9`)
+tiles.setTileAt(tiles.getTileLocation(9, 5), assets.tile`myTile10`)
 mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.One), cursor)
 let cursor2 = sprites.create(img`
     . . . . . . . . . . . . . . . . 
@@ -65,11 +86,11 @@ let cursor2 = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . f f f f f . . . . . 
-    . . . . . f . . . . . f . . . . 
-    . . . . . f . . . . . f . . . . 
-    . . . . . f . . . . . f . . . . 
-    . . . . . f . . . . . f . . . . 
-    . . . . . f . . . . . f . . . . 
+    . . . . . f f f f f f f . . . . 
+    . . . . . f f f f f f f . . . . 
+    . . . . . f f f f f f f . . . . 
+    . . . . . f f f f f f f . . . . 
+    . . . . . f f f f f f f . . . . 
     . . . . . . f f f f f . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
--- tilemap.g.ts
+++ tilemap.g.ts
@@ -9,11 +9,23 @@ namespace myTiles {
     //% fixedInstance jres blockIdentity=images._tile
     export const tile4 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
+    export const tile5 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
     export const tile6 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
     export const tile7 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
     export const tile8 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile9 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile10 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile11 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile12 = image.ofBuffer(hex``);
 
     helpers._registerFactory("tilemap", function(name: string) {
         switch(helpers.stringTrim(name)) {
@@ -103,12 +115,24 @@ namespace myTiles {
             case "tile3":return tile3;
             case "myTile2":
             case "tile4":return tile4;
+            case "myTile3":
+            case "tile5":return tile5;
             case "myTile4":
             case "tile6":return tile6;
             case "myTile5":
             case "tile7":return tile7;
             case "myTile6":
             case "tile8":return tile8;
+            case "myTile7":
+            case "tile9":return tile9;
+            case "myTile":
+            case "tile1":return tile1;
+            case "myTile8":
+            case "tile10":return tile10;
+            case "myTile9":
+            case "tile11":return tile11;
+            case "myTile10":
+            case "tile12":return tile12;
         }
         return null;
     })
